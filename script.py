2bc71e2460fd70e50e45b5ea905f210d49c8b0dd
Jeron Lloyd <lloydjaj@gmail.com>
2019-05-29 15:26:57 -0300

    Removed some code and added new print statement

--- script.py
+++ script.py
@@ -1,17 +1,8 @@
-import sys
+
 import requests
-from os import rename
-import math
-
-print (sys.version)
-print(sys.executable)
-
-
-def greet(hi_greet):
-   
-    greeting = "hello, {}".format(hi_greet)
-    return greeting
 
 
 r = requests.get("https://coreyms.com")
 print(r.status_code)
+print(r.ok)
+
